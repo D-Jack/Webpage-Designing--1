d0a28a90c5511b202b14cf75be91081b972e24dc
D-Jack <siddharthauchiha50@gmail.com>
2021-02-06 22:04:08 +0530

    making site responsive

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,5 @@
+const backDropEl = document.getElementById("backdrop");
+
 const navScrollHandler = () => {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     document.getElementById("mainNav").classList.add("nav-responsive");
@@ -9,7 +11,24 @@ const navScrollHandler = () => {
 window.onscroll = () => {
   navScrollHandler();
 };
+
+
+const toggleBackdropHandler = () => {
+  backDropEl.classList.toggle("visible");
+};
+const backdrophandler = () => {
+  toggleNavbar();
+  toggleBackdropHandler();
+};
+
 const toggleNavbar=()=> {
   const navEl = document.getElementById("mainNav");
   navEl.classList.toggle("responsive");
 }
+
+const navbarHandler = () => {
+  toggleNavbar();
+  toggleBackdropHandler();
+}
+
+backDropEl.addEventListener("click", backdrophandler);
